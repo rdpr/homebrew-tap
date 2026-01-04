d32ccf30ed52879430405bc7c8d8c8208d1622e2
Ruben Roberto <ruben.d.roberto@gmail.com>
2026-01-04 02:12:34 +0000

    remove appcast

--- Casks/ableton-live-standard@10.rb
+++ Casks/ableton-live-standard@10.rb
@@ -3,13 +3,15 @@ cask "ableton-live-standard@10" do
   sha256 "73c04b069f9636931ed585e8c936276a33ea4f24871ae494542739926cd15fa2"
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_64.dmg"
-  appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
   name "Ableton Live Standard"
+  desc "Sound and music editor"
   homepage "https://www.ableton.com/en/live/"
 
   auto_updates true
 
   app "Ableton Live #{version.major} Standard.app"
+
+  uninstall quit: "com.ableton.live"
 
   zap trash: "~/Library/*/*[Aa]bleton*",
       rmdir: "~/Music/Ableton/Factory Packs"
